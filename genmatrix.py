221f7676427946729c4c2a5f73e6b353194a431a
Joel Frederico <joelfred@slac.stanford.edu>
2011-08-03 15:09:39 -0700

    Added documentation for genmatrix.py

--- genmatrix.py
+++ genmatrix.py
@@ -5,36 +5,60 @@ import os
 import sys
 import csv
 import makepar
-from math import cos, sin, sqrt
 
-k=123.4567901
+from math import cos, sin, sqrt, tan
+
+# Defines division length
 delL=0.1
 
-def rollup(x,a):
-	return a(1)+a(2)*tanh(a(3)*x+a(4))
 
-def sqrtk(L):
+# Returns sqrt(k) where k is the focusing,
+# as a function of s-coordinate.
+def k(L):
 	# Frac of length
 	temp=L/(128*delL)
-	a=[1.006370764,0.9640900469, 0.3349204039, -6.217212044]
-	return sqrt(rollup(x,a))
+	This defines the 
+	return L*(1-temp)
 
 def genmatrix():
+	# Open and determine how to write to file
 	fID=open('qmat.csv','w')
 	csv.register_dialect('csv',delimiter=',',skipinitialspace=True)
 	wrtr=csv.writer(fID,dialect='csv')
+
+	# Header row for parameters to be written
 	wrtr.writerow(['','Occurence','L','R11','R33','R12','R34','R21','R43','R22','R44'])
+
 	for i in range(1,129):
-		effrtk=sqrtk(delL*(i-1))
-		r11=cos(effrtk*delL)
-		r12=sin(effrtk*delL)/effrtk
-		r21=-effrtk*sin(effrtk*delL)
-		r22=cos(effrtk*delL)
+		# Current s coordinate
+		currentL = delL*(i-1)
+
+		# Effective square root of focusing
+		# to be used in current section, k:
+		# x'' = k(s) x
+		effrtk   = math.sqrt(k(currentL))
+
+		# Thick lens matrix elements.
+		# Note: r(1:2,1:2)=r(3:4,3:4)
+		r11 = math.cos(effrtk*delL)
+		r12 = math.sin(effrtk*delL)/effrtk
+		r21 = -effrtk*math.sin(effrtk*delL)
+		r22 = math.cos(effrtk*delL)
+
+		# Row of parameter values to be written to CSV
+		# (for tranlation to .par)
 		row=['Q',i,delL,r11,r11,r12,r12,r21,r21,r22,r22]
+
+		# Write to file
 		wrtr.writerow(row)
+
+	# Close up file
 	fID.close()
+
+	# Translate file to .par
 	makepar.makepar('qmat.csv','qmat.par',False)
 
+# Executed when called from command line
 if __name__ == '__main__':
 	parser=argparse.ArgumentParser(description=
 			'Generates matrix file for many-quad simulation of plasma lensing.')
